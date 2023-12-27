0b064ad6cabec4e85d71c67cf3223a92966a3ed1
Hrashikesh <hrashikesh87@gmail.com>
2023-12-27 19:50:06 +0530

    Cnova update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            Crinova™
+            Cnova™
           </a>
           . All Rights Reserved.
         </span>
